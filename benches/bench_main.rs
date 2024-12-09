b8dd207b87dd2fb42cb4d36f5dc700970bf2b693
Lucas de Linhares <lucasdelinhares@gmail.com>
2024-12-09 12:50:21 +0000

    Rename files to remove the _bench suffix

--- benches/bench_main.rs
+++ benches/bench_main.rs
@@ -3,7 +3,7 @@ use criterion::criterion_main;
 mod benchmarks;
 
 criterion_main!(
-    benchmarks::argon2_bench::benches,
-    benchmarks::rust_argon2_bench::benches,
-    benchmarks::argon2_kdf_bench::benches
+    benchmarks::argon2::benches,
+    benchmarks::rust_argon2::benches,
+    benchmarks::argon2_kdf::benches
 );
